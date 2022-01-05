e79068ddb1477ba554f0b3b59600eb103737810a
Sergey Kondratenko <Sergey.Kondratenko@ing.com>
2022-01-05 11:36:34 +0100

    introduce nl format

--- src/locale/locale.js
+++ src/locale/locale.js
@@ -1,10 +1,11 @@
 import en from './en'
 import zh from './zh'
 import es from './es'
+import nl from './nl'
 import zh_tw from './zh_tw'
 import Store from '../store';
 
-const localeObj = {en,zh,es,zh_tw}
+const localeObj = {en,zh,es,zh_tw,nl}
 
 function locale(){
     return localeObj[Store.lang];
